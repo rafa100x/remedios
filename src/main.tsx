6a97231c53775097f60d44f9b39392ce6f5afc09
RAFA <rafaelvidetta32@gmail.com>
2026-04-23 23:19:05 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import './index.css';
-import ReactGA from 'react-ga4';
-const gaMeasurementId = 'G-TH26DT24Y2';
-ReactGA.initialize(gaMeasurementId);
+
+// Inicializar Google Analytics con tu ID proporcionado
+ReactGA.initialize('G-TH26DT24Y2');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
