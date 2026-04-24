0d60889dfb1c4c48c41b2303111288e15f06f38c
RAFA <rafaelvidetta32@gmail.com>
2026-04-23 23:15:41 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import './index.css';
+import ReactGA from 'react-ga4';
+const gaMeasurementId = 'G-TH26DT24Y2';
+ReactGA.initialize(gaMeasurementId);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
